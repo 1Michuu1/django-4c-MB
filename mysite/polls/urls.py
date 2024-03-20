0f59542616b633d7cfc3735ebc64b87781c22fed
Michu <wewwe@wp.pl>
2024-03-20 08:38:38 +0100

    20.03.24

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
-    path("counter/", views.counter, name="")
-]
-
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("calculator/", views.calculator, name="")
+    # ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("calculator/", views.calculator, name=""),
+    path("counter/", views.counter, name=""),
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
 ]
